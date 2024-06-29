7e42715d7de8b44cfb54c3ea241a9ba9ddda4476
BharathrajShetty <136963114+BharathrajShetty@users.noreply.github.com>
2024-06-29 11:33:35 +0000

    Added more pages to the website

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-import database 
+from flask import Flask, render_template, jsonify, request
+import database
 
 app = Flask(__name__)
 
@@ -9,17 +9,35 @@ def hello_world():
     jobs = database.load_jobs_from_db()
     return render_template('home.html', jobs=jobs)
 
+@app.route("/jobs")
+def display_jobs():
+    jobs = database.load_jobs_from_db()
+    return render_template('pages/jobs_page.html', jobs=jobs)
+
+
+@app.route("/overview")
+def get_overview():
+    return render_template('pages/overview.html')
+
+@app.route("/faqs")
+def get_faqs():
+    faqs = database.get_faqs_from_db()
+    return render_template('pages/faqs.html', faqs=faqs)
+    
+##############################################################################
+# Api's 
+##############################################################################
+@app.route("/api/job")
+def load_job():
+    job_id = request.args['job_id']
+    job_details = database.get_job_details(job_id)
+    return jsonify(job_details)
+
 
 @app.route("/api/jobs")
 def list_jobs():
     jobs = database.load_jobs_from_db()
     return jsonify(jobs)
-
-
-@app.route("/overview")
-def get_overview():
-    return render_template('overview.html')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 import pymysql
 import os
 
-
-def load_jobs_from_db():
+def db_connection():
     timeout = 10
     connection = pymysql.connect(
         charset="utf8mb4",
@@ -16,15 +15,30 @@ def load_jobs_from_db():
         user= os.environ['USER'],
         write_timeout=timeout,
     )
+    return connection
 
+def exec_simple_db_queries(query):
+    connection = db_connection()
     try:
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM jobs")
+        cursor.execute(query)
         return cursor.fetchall()
     except Exception as e:
         return str(e)
     finally:
         connection.close()
+    
+def load_jobs_from_db():
+    query = "SELECT * FROM jobs"
+    return exec_simple_db_queries(query)
 
+def get_job_details(job_id):
+    query = f"SELECT * FROM jobs where id = {job_id}"
+    return exec_simple_db_queries(query)
+
+def get_faqs_from_db():
+    query = "SELECT * FROM faqs"
+    return exec_simple_db_queries(query)
+    
 if __name__ == "__main__":
     print(load_jobs_from_db())
