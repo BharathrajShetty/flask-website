2ced534a2c084e0dd49f1daf748a6babed7635a0
BharathrajShetty <136963114+BharathrajShetty@users.noreply.github.com>
2024-06-30 19:57:16 +0000

    Added login page

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session
 import database
+import secrets
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+app.permanent_session_lifetime = timedelta(days=1)
+
+
+@app.route("/loginPage")
+def login_page():
+    return render_template("loginPage.html")
+
+
+@app.route("/user", methods=['post'])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    session.permanent = True
+    session['user_name'] = username
+    return {'status': 'success', 'user': username}
+    # user = database.get_user_by_username(username)
+    # if user and user["password"] == password:
+    #     session["user_id"] = user["id"]
+    #     return jsonify({"success": True})
+    # else:
+    #     return jsonify({"success": False, "message": "Invalid username or password"})
 
 
 @app.route("/")
